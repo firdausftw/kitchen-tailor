258b76729910e5786dbcbf549d2ed7e066bbd1a6
firdausftw <mfirdaus.madnan@gmail.com>
2026-04-20 23:23:54 +0800

    install sass, bootstrap, manrope font

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div id="home">
+    <div id="home-banner" class="container-fluid px-lg-0 px-4 d-flex">
+        <div class="custom-container">
+            <div class="row justify-content-center">
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
