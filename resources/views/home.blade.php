3d78f712bddefe10f04562b19b2559faf0e456f2
firdausftw <mfirdaus.madnan@gmail.com>
2026-04-29 00:50:47 +0800

    add images for kitchen, wardrobe, bathroom

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,11 @@
             <div class="row custom-container kitchen-tailor-home-container align-items-center">
                 <div class="fade-slider">
                     <img src="/images/kitchen-tailor/Homepage-Banner-8.jpg" class="slide active">
+                    <img src="/images/kitchen-tailor/bathroom/Bathroom-4.jpeg" class="slide">
+                    <img src="/images/kitchen-tailor/kitchen/Kitchen-2.jpeg" class="slide">
+                    <img src="/images/kitchen-tailor/bathroom/Bathroom-7.jpeg" class="slide">
+                    <img src="/images/kitchen-tailor/kitchen/Kitchen-5.jpeg" class="slide">
+                    <img src="/images/kitchen-tailor/bathroom/Bathroom-9.jpeg" class="slide">
                     <img src="/images/kitchen-tailor/Homepage-Banner-9.jpg" class="slide">
                 </div>
             </div>
@@ -36,19 +41,19 @@
             <div class="row custom-container kitchen-tailor-fade-container">
                 <div class="col-12 d-flex flex-column flex-md-row gap-2 gap-md-0 justify-content-between">
                     <div class="col-12 col-md-4 fade-wrapper">
-                        <img src="/images/kitchen-tailor/Homepage-Banner-1.jpg" class="fade-img">
+                        <img src="/images/kitchen-tailor/kitchen/Kitchen-3.jpeg" class="fade-img">
                         <div class="fade-overlay"></div>
-                        <div class="gothic-font fade-text">THE INSIGHTS</div>
+                        <div class="gothic-font fade-text">KITCHEN</div>
                     </div>
                     <div class="col-12 col-md-4 fade-wrapper">
-                        <img src="/images/kitchen-tailor/Homepage-Banner-3.jpg" class="fade-img">
+                        <img src="/images/kitchen-tailor/bathroom/Bathroom-3.jpeg" class="fade-img">
                         <div class="fade-overlay"></div>
-                        <div class="gothic-font fade-text">THE KITCHEN</div>
+                        <div class="gothic-font fade-text">BATHROOM</div>
                     </div>
                     <div class="col-12 col-md-4 fade-wrapper">
-                        <img src="/images/kitchen-tailor/Homepage-Banner-4.jpg" class="fade-img">
+                        <img src="/images/kitchen-tailor/wardrobe/Wardrobe-1.jpeg" class="fade-img">
                         <div class="fade-overlay"></div>
-                        <div class="gothic-font fade-text">THE COMPLETE HOME</div>
+                        <div class="gothic-font fade-text">WARDROBE</div>
                     </div>
                 </div>
             </div>
