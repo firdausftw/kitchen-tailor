541236778e6652e78d08038b03b7a74ccf5f7483
firdausftw <mfirdaus.madnan@gmail.com>
2026-04-30 09:25:33 +0800

    add designer section in GerobokCendayam page

--- resources/views/collaborations/gerobok-cendayam.blade.php
+++ resources/views/collaborations/gerobok-cendayam.blade.php
@@ -19,6 +19,53 @@
             </div>
         </div>
 
+        <div id="kitchen-tailor-designer-1" class="container-fluid px-lg-0 px-4">
+            <div class="row custom-container gerobok-designer-container">
+                <div class="col-12 d-flex flex-column flex-lg-row gap-2">
+                    <div class="col-12 col-lg-5 collaboration-img-wrapper">
+                        <div class="designer-wrapper">
+                            <img src="/images/kitchen-tailor/IDr-Norshafina.png" class="gerobok-img img-designer">
+                            <div class="fade-overlay collaborations-content-overlay gerobok-cendayam-overlay"></div>
+                            <div class="gerobok-cendayam-text-box designer-text-box d-flex flex-column justify-content-end align-items-center">
+                                <div class="gothic-font-big fade-text gerobok-cendayam-text text-title">IDr. Norshafina Binti Ibrahim</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-7 font-kitchen-tailor-black px-4">
+                        <h2 class="gothic-font-big font-designer text-center text-lg-start pt-4 pt-lg-0">Meet the</h2>
+                        <h2 class="gothic-font-big font-designer text-center text-lg-start font-kitchen-tailor">Designer</h2>
+                        <div class="d-flex flex-column pt-4 font-kitchen-tailor-black">
+                            <h2 class="gothic-font-small font-designer-content-text">
+                                Norshafina, a graduate of MARA University of Technology in Interior Architecture, began her career by gaining experience with renowned design firms in Kuala Lumpur, working on hospitality and institutional projects
+                            </h2>
+                            <br>
+                            <h2 class="gothic-font-small font-designer-content-text">
+                                In 2003, at the age of 25, she co-founded Skala Design Consult Sdn Bhd, where she now serves as CEO. Under her leadership, Skala has become a leading interior design practise in Malaysia, specialising in public spaces and institutional design. The firm's portfolio includes iconic projects such as the Parliament House, KLIA T2, Seri Perdana and The National Palace. Skala has been recognised with numerous awards, including the REKA Awards, International Property Awards, and the Kuala Lumpur Mayor Award for Best Interior Design.
+                            </h2>
+                        </div>
+                        <div class="d-flex flex-column justify-content-center align-items-start gap-4 pt-4">
+                            <div class="d-flex gap-4">
+                                <i class="fa-solid fa-envelope font-kitchen-tailor-icon inquiry-icon"></i>
+                                <h2 class="gothic-font-small gerobok-cendayam-center-bottom-text">fina@skaladc.com.my</h2>
+                            </div>
+                            <div class="d-flex gap-4">
+                                <i class="fa-solid fa-globe font-kitchen-tailor-icon inquiry-icon"></i>
+                                <h2 class="gothic-font-small gerobok-cendayam-center-bottom-text">www.skaladc.com.my</h2>
+                            </div>
+                            <div class="d-flex gap-4">
+                                <i class="fa-brands fa-facebook-f font-kitchen-tailor-icon inquiry-icon"></i>
+                                <h2 class="gothic-font-small gerobok-cendayam-center-bottom-text">facebook.com/fina.ibrahim.9</h2>
+                            </div>
+                            <div class="d-flex gap-4">
+                                <i class="fa-brands fa-instagram font-kitchen-tailor-icon inquiry-icon"></i>
+                                <h2 class="gothic-font-small gerobok-cendayam-center-bottom-text">instagram.com/norshafinaibrahim1</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div id="gerobok-cendayam" class="container-fluid px-lg-0 px-4">
             <div class="row custom-container gerobok-cendayam-img-container">
                 <div class="col-12 d-flex flex-column flex-lg-row justify-content-center align-items-start gap-2">
